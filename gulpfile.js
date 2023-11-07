b8c1581d52b240042d6b21a91e6d3a09de5d920e
Cristin <tianncristiann@gmail.com>
2023-11-07 04:06:47 +0200

    add top-slider section
    add top-slider section

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,11 @@ function styles() {
 }
 
 function script() {
-	return src(['node_modules/jquery/dist/jquery.js', 'app/js/main.js'])
+	return src([
+		'node_modules/jquery/dist/jquery.js',
+		'node_modules/slick-carousel/slick/slick.js',
+		'app/js/main.js',
+	])
 		.pipe(concat('main.min.js'))
 		.pipe(uglify())
 		.pipe(dest('app/js'))
